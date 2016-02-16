fb85dc38cd99db5611252f46becf17fd62465ea7
Emclee1 <emclee@my.waketech.edu>
2016-02-16 16:55:21 -0500

    AccessTest Write to DB
    Added code to write to the file. The text value fields have an error
    where the length has to be exactly 30 or 15, will look into later.

--- AccessDBTest/src/Test.java
+++ AccessDBTest/src/Test.java
@@ -16,17 +16,44 @@ public class Test
 		Connection con;
 		try 
 		{
-			con = DriverManager.getConnection("jdbc:ucanaccess://" + System.getProperty("user.dir") + "\\src\\Recipe_test.accdb");
+			con = DriverManager.getConnection( "jdbc:ucanaccess://" + System.getProperty("user.dir") + "\\src\\Recipe_test.accdb" );
 			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM Recipe");
-			
+			int id = getLastID( st );
+			st.executeUpdate( "INSERT INTO Recipe " +
+					" VALUES( " + id + ", 'FishFishFishFishFishFishFishFi', 'moderatemoderat' ) " ); //Problem with length
+
+			ResultSet rs = st.executeQuery( "SELECT * FROM Recipe" );
 			while(rs.next())
 			{
-				System.out.println(rs.getString("rec_ID") + " " + rs.getString("rec_name") + " " + rs.getString("difficulty"));
+				System.out.println(rs.getString( "rec_ID" ) + " " + rs.getString( "rec_name" ) + " " + rs.getString( "difficulty" ) );
 			}
+			con.commit();
+			con.close();
 		} catch (SQLException e) 
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	public static int getLastID( Statement st )
+	{
+		int id = -1;
+		
+		try 
+		{
+			ResultSet rs = st.executeQuery( "SELECT rec_ID FROM Recipe" );
+			
+			while(rs.next())
+			{
+				if( rs.isLast() )
+				{
+					id = rs.getInt( "rec_ID" );
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return id + 1;
 	}
 }
